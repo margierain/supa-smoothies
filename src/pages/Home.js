ecd81d46b59e1eec7d1c36c7bc1c86862346c896
Margaret Ochieng <margaretrain.mo@gmail.com>
2025-08-20 21:51:51 +0300

    fetch and order data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
   console.log(supabase);
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState([])
+  const [orderBy, setOrderBy] = useState('created_at')
 
   const handleDelete = (id) => {
     setSmoothies(prevSmoothies => {
@@ -21,6 +22,7 @@ const Home = () => {
       const { data, error } = await supabase
         .from('smoothies')
         .select('*')
+        .order(orderBy, {ascending: false})
 
       if (error) {
         setFetchError('Could not fetch the smoothies')
@@ -34,7 +36,7 @@ const Home = () => {
       }
     }
     fetchSmoothies()
-  }, [])
+  }, [orderBy])
 
 
   return (
@@ -44,12 +46,18 @@ const Home = () => {
       }
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p> Order By:</p>
+            <button onClick={() => setOrderBy('created_at')}> Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothies => (
-              <SmoothieCard 
-              key={smoothies.id} 
-              smoothies={smoothies}
-              onDelete={handleDelete} />
+              <SmoothieCard
+                key={smoothies.id}
+                smoothies={smoothies}
+                onDelete={handleDelete} />
             ))}
           </div>
         </div>)}
